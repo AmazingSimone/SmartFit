15cca34e79eb5cc743fde06b870fa17e62de2e5a
AmazingSimone <simonboss608@gmail.com>
2024-11-16 20:15:32 +0100

    Pridanie dodatocnych parametrov pouzivatela

--- app/src/main/java/com/example/smartfit/data/User.kt
+++ app/src/main/java/com/example/smartfit/data/User.kt
@@ -1,7 +1,13 @@
 package com.example.smartfit.data
 
+import java.time.LocalDateTime
+
 data class User(
     val id: String? = "",
     val displayName: String? = "",
-    val profilePicUrl: String? = null
+    val profilePicUrl: String? = null,
+    val birthDate: Long? = 0L,
+    val height: Float? = 0f,
+    val weight: Float? = 0f,
+    val bio: String? = ""
 )
