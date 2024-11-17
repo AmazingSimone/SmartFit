43ebe41d0b8883748b22615a4b92e7633fcc6594
AmazingSimone <simonboss608@gmail.com>
2024-11-17 22:19:15 +0100

    V objekte User som zmenil atributy z nullable na normalne, neviem preco som to tak nastavoval v prvom rade...

--- app/src/main/java/com/example/smartfit/data/User.kt
+++ app/src/main/java/com/example/smartfit/data/User.kt
@@ -4,13 +4,13 @@ import androidx.compose.ui.graphics.Color
 import java.time.LocalDateTime
 
 data class User(
-    val id: String? = "",
-    val displayName: String? = "",
-    val profilePicUrl: String? = null,
-    val birthDate: Long? = 0L,
-    val height: Float? = 0f,
-    val weight: Float? = 0f,
-    val bio: String? = "",
+    val id: String = "",
+    val displayName: String = "",
+    val profilePicUrl: String = "",
+    val birthDate: Long = 0L,
+    val height: Float = 0f,
+    val weight: Float = 0f,
+    val bio: String = "",
     val color: Color = Color.Unspecified,
     val isTrainer: Boolean = false
 )
