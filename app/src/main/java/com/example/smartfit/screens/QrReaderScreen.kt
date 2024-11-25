4c216a4d7a22b635c2eed2cf7c6338e5108f7474
AmazingSimone <simonboss608@gmail.com>
2024-11-25 16:26:49 +0100

    Doupravil som obrazovku so skenovanim qr kodu aby zatial len zobrazovala vlastny alert dialog s precitanym treningom

--- app/src/main/java/com/example/smartfit/screens/QrReaderScreen.kt
+++ app/src/main/java/com/example/smartfit/screens/QrReaderScreen.kt
@@ -1,10 +1,14 @@
+import android.Manifest
+import android.os.Build
 import android.os.Handler
 import android.os.Looper
-import android.widget.Toast
+import androidx.annotation.RequiresApi
 import androidx.camera.core.CameraSelector
 import androidx.camera.core.ImageAnalysis
 import androidx.camera.core.ImageProxy
+import androidx.camera.core.Preview
 import androidx.camera.lifecycle.ProcessCameraProvider
+import androidx.camera.view.PreviewView
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
@@ -18,6 +22,9 @@ import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.viewinterop.AndroidView
 import androidx.concurrent.futures.await
 import androidx.lifecycle.compose.LocalLifecycleOwner
+import com.example.smartfit.components.CustomAlertDialogGroupTraining
+import com.example.smartfit.data.GroupTraining
+import com.example.smartfit.data.User
 import com.google.accompanist.permissions.ExperimentalPermissionsApi
 import com.google.accompanist.permissions.isGranted
 import com.google.accompanist.permissions.rememberPermissionState
@@ -32,32 +39,62 @@ import java.util.concurrent.ExecutorService
 import java.util.concurrent.Executors
 
 
+@RequiresApi(Build.VERSION_CODES.O)
 @OptIn(ExperimentalPermissionsApi::class)
 @Composable
-fun QrReaderScreen() {
+fun QrReaderScreen(
+    onResult: (String) -> Unit,
+    foundGroupTraining: GroupTraining = GroupTraining(),
+    foundTrainer: User = User()
+) {
     val context = LocalContext.current
     val cameraProviderFuture = remember { ProcessCameraProvider.getInstance(context) }
     val cameraExecutor: ExecutorService = remember { Executors.newSingleThreadExecutor() }
     val lifecycleOwner = LocalLifecycleOwner.current
     val cameraSelector = CameraSelector.DEFAULT_BACK_CAMERA
-    val permissionState = rememberPermissionState(android.Manifest.permission.CAMERA)
+    val permissionState = rememberPermissionState(Manifest.permission.CAMERA)
     var cameraProvider by remember { mutableStateOf<ProcessCameraProvider?>(null) }
+
+    val openAlertDialog = remember { mutableStateOf(false) }
+
 
     LaunchedEffect(cameraProviderFuture) {
         cameraProvider = cameraProviderFuture.await()
+    }
+
+
+//    if ((foundTrainer != User()) && (foundGroupTraining != GroupTraining())) {
+//        Log.d("ahoj", "tu som: $foundGroupTraining, $foundTrainer")
+//        openAlertDialog.value = true
+//    }
+    when {
+        openAlertDialog.value -> {
+            CustomAlertDialogGroupTraining(
+                onDismissRequest = {
+
+                    openAlertDialog.value = false
+                },
+                onConfirmation = {
+                    openAlertDialog.value = false
+                },
+                groupTraining = foundGroupTraining,
+                trainer = foundTrainer
+            )
+        }
     }
 
     if (permissionState.status.isGranted) {
         Box(modifier = Modifier.fillMaxSize()) {
             cameraProvider?.let { provider ->
                 AndroidView(factory = { ctx ->
-                    val previewView = androidx.camera.view.PreviewView(ctx)
-                    val preview = androidx.camera.core.Preview.Builder().build().also {
+                    val previewView = PreviewView(ctx)
+                    val preview = Preview.Builder().build().also {
                         it.surfaceProvider = previewView.surfaceProvider
                     }
                     val imageAnalyzer = ImageAnalysis.Builder().build().also {
                         it.setAnalyzer(cameraExecutor, QrCodeAnalyzer { result ->
-                            Toast.makeText(ctx, result, Toast.LENGTH_SHORT).show()
+                            onResult(result)
+                            openAlertDialog.value = true
                         })
                     }
                     provider.bindToLifecycle(
