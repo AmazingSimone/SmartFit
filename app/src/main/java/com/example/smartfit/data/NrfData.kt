85ac38632fce12d31d5244d8b9b5ce20433ee440
AmazingSimone <simonboss608@gmail.com>
2024-12-14 13:19:29 +0100

    Pridal som novy datovy objekt ktory bude uchovavat aktualne prijate data z pasu a jeho hodnoty v nom sa pravdepodobne budu stale menit...

---
+++ app/src/main/java/com/example/smartfit/data/NrfData.kt
@@ -0,0 +1,13 @@
+package com.example.smartfit.data
+
+data class NrfData(
+    val tep: String = "0",
+    val teplota: String = "0.0",
+    val steps: String = "0",
+    val kadencia: String = "0",
+    val spaleneKalorie: String = "0",
+    val vzdialenost: String = "0",
+    val saturacia: String = "0.0",
+    val rychlost: String = "0",
+    val vo2Max: String = "0"
+)
