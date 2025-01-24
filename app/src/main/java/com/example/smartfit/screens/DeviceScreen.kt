c1069e2c14537f8c626fbbac546d65de7bf3bcc0
Simone <simonboss608@gmail.com>
2025-01-24 14:14:08 +0100

    Nastavil som fixnu velkost vyskakovacieho dialogu ak najde viacej zariadeni v obrazovke pre pripojenie na ble zariadenie

--- app/src/main/java/com/example/smartfit/screens/DeviceScreen.kt
+++ app/src/main/java/com/example/smartfit/screens/DeviceScreen.kt
@@ -7,6 +7,7 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.IntrinsicSize
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
@@ -66,7 +67,9 @@ fun DeviceScreen(
                 dialogTitle = "Najdene vysledky",
                 dialogContent = {
                     if (bleListOfDevices?.isNotEmpty() == true) {
-                        LazyColumn {
+                        LazyColumn(
+                            modifier = Modifier.height(200.dp)
+                        ) {
                             items(bleListOfDevices) { device ->
                                 ListItem(
                                     headlineContent = {
