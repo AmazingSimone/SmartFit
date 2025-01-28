06c3b58e0bec3023e32481faec239b71f88b67ce
Simone <simonboss608@gmail.com>
2025-01-28 16:50:11 +0100

    Zadane hodnoty pri vytvarani skup. treningu sa uz uchovavaju pri rozsiahlej zmene kompozicie, jemne som upratal usporiadanie prvkov v back-end a pridal som upozornovacie okno ktore varuje trenera o strate zadanych dat pri nevytvoreni treningu

--- app/src/main/java/com/example/smartfit/screens/CreateGroupTrainingScreen.kt
+++ app/src/main/java/com/example/smartfit/screens/CreateGroupTrainingScreen.kt
@@ -1,11 +1,13 @@
 package com.example.smartfit.screens
 
 import android.os.Build
+import androidx.activity.compose.BackHandler
 import androidx.annotation.RequiresApi
-import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
@@ -14,6 +16,7 @@ import androidx.compose.material.icons.automirrored.filled.ArrowBack
 import androidx.compose.material.icons.filled.Edit
 import androidx.compose.material.icons.filled.EditNote
 import androidx.compose.material.icons.filled.People
+import androidx.compose.material.icons.filled.Warning
 import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.ExposedDropdownMenuBox
@@ -32,6 +35,7 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -40,6 +44,7 @@ import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.example.smartfit.components.CustomAlertDialog
 import com.example.smartfit.components.CustomButton
 import com.example.smartfit.components.CustomOutlinedTextInput
 import com.example.smartfit.components.Heading1
@@ -55,34 +60,56 @@ import java.time.ZoneOffset
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun CreateGroupTrainingScreen(
-    trainerId: String = "sdv6s4v6s4dv66sdv4",
+    trainerId: String = "",
     onBackClick: () -> Unit,
     onCreateTraining: (GroupTraining) -> Unit
 ) {
 
-    val nameValue = remember {
+    val nameValue = rememberSaveable {
         mutableStateOf("")
     }
 
-    val maxUsersValue = remember {
+    val maxUsersValue = rememberSaveable {
         mutableIntStateOf(0)
     }
+    val maxUsersString = rememberSaveable {
+        mutableStateOf("0")
+    }
 
-    val detailsValue = remember {
+    val detailsValue = rememberSaveable {
         mutableStateOf("")
     }
 
     val trainings: List<Training> = trainingList
-    var expanded by remember { mutableStateOf(false) }
-    var chosenTrainingIndex by remember { mutableIntStateOf(0) }
-    val textFieldState = remember { mutableStateOf("") }
+    var expanded by rememberSaveable { mutableStateOf(false) }
+    var chosenTrainingIndex by rememberSaveable { mutableIntStateOf(0) }
+    val textFieldState = rememberSaveable { mutableStateOf("") }
+
+    val showAlertDialog = rememberSaveable { mutableStateOf(false) }
+
+    val validator =
+        { nameValue.value.isNotEmpty() || maxUsersValue.intValue > 0 || detailsValue.value.isNotEmpty() || textFieldState.value != "" }
+
+    BackHandler {
+        if (validator()) {
+            showAlertDialog.value = true
+        } else {
+            onBackClick()
+        }
+    }
 
     Scaffold(
         topBar = {
             TopAppBar(
                 title = { Heading1("Vytvor skupinovy trening") },
                 navigationIcon = {
-                    IconButton(onClick = { onBackClick() }) {
+                    IconButton(onClick = {
+                        if (validator()) {
+                            showAlertDialog.value = true
+                        } else {
+                            onBackClick()
+                        }
+                    }) {
                         Icon(
                             imageVector = Icons.AutoMirrored.Filled.ArrowBack,
                             contentDescription = "Back icon"
@@ -119,139 +146,150 @@ fun CreateGroupTrainingScreen(
 
         val (nameOfTraining, maxUsers, detailsOfTraining) = remember { FocusRequester.createRefs() }
 
-        Surface {
+        Surface(modifier = Modifier.padding(paddingValues)) {
 
-
-            Box(
+            Column(
                 modifier = Modifier
-                    .fillMaxSize()
-                    .padding(horizontal = 20.dp),
-                contentAlignment = Alignment.TopCenter
+                    .padding(horizontal = 20.dp)
+                    .verticalScroll(rememberScrollState()),
+                horizontalAlignment = Alignment.CenterHorizontally
             ) {
 
-                Column(
-                    modifier = Modifier.verticalScroll(rememberScrollState()),
-                    horizontalAlignment = Alignment.CenterHorizontally
+                Spacer(Modifier.padding(6.dp))
+
+
+                CustomOutlinedTextInput(
+                    currentFocusRequester = nameOfTraining,
+                    onNext = { nameOfTraining.freeFocus() },
+                    keyBoardType = KeyboardType.Text,
+                    label = "Nazov treningu",
+                    value = nameValue.value,
+                    enterButtonAction = ImeAction.Default,
+                    onTextChanged = { nameValue.value = it },
+                    supportingText = {
+                        NormalText("Nepovinne")
+                    },
+                    trailingIcon = {
+                        Icon(
+                            imageVector = Icons.Filled.Edit,
+                            contentDescription = "Training name icon"
+                        )
+                    }
+                )
+
+                Spacer(Modifier.padding(6.dp))
+
+                ExposedDropdownMenuBox(
+                    expanded = expanded,
+                    onExpandedChange = { expanded = it },
                 ) {
 
-                    Spacer(Modifier.padding(40.dp))
-
-
                     CustomOutlinedTextInput(
+                        modifier = Modifier.menuAnchor(MenuAnchorType.PrimaryNotEditable, true),
+                        value = textFieldState.value,
                         currentFocusRequester = nameOfTraining,
-                        onNext = { nameOfTraining.freeFocus() },
-                        keyBoardType = KeyboardType.Text,
-                        label = "Nazov treningu",
-                        value = nameValue.value,
-                        enterButtonAction = ImeAction.Default,
-                        onTextChanged = { nameValue.value = it },
-                        supportingText = {
-                            NormalText("Nepovinne")
-                        },
-                        trailingIcon = {
-                            Icon(
-                                imageVector = Icons.Filled.Edit,
-                                contentDescription = "Training name icon"
-                            )
-                        }
+                        label = "Zvol typ treningu",
+                        onTextChanged = { textFieldState.value = it },
+                        readOnly = true,
+                        singleLine = true,
+                        trailingIcon = { ExposedDropdownMenuDefaults.TrailingIcon(expanded = expanded) }
                     )
 
-                    Spacer(Modifier.padding(6.dp))
-
-                    ExposedDropdownMenuBox(
+                    ExposedDropdownMenu(
                         expanded = expanded,
-                        onExpandedChange = { expanded = it },
+                        onDismissRequest = { expanded = false },
                     ) {
-
-                        CustomOutlinedTextInput(
-                            modifier = Modifier.menuAnchor(MenuAnchorType.PrimaryNotEditable, true),
-                            value = textFieldState.value,
-                            currentFocusRequester = nameOfTraining,
-                            label = "Zvol typ treningu",
-                            onTextChanged = { textFieldState.value = it },
-                            readOnly = true,
-                            singleLine = true,
-                            trailingIcon = { ExposedDropdownMenuDefaults.TrailingIcon(expanded = expanded) }
-                        )
-
-                        ExposedDropdownMenu(
-                            expanded = expanded,
-                            onDismissRequest = { expanded = false },
-                        ) {
-                            trainings.forEach { training ->
-                                DropdownMenuItem(
-                                    text = {
-                                        Text(
-                                            training.name,
-                                            style = MaterialTheme.typography.bodyLarge
-                                        )
-                                    },
-                                    onClick = {
-                                        textFieldState.value = training.name
-                                        chosenTrainingIndex = trainings.indexOf(training)
-                                        expanded = false
-                                    },
-                                    contentPadding = ExposedDropdownMenuDefaults.ItemContentPadding,
-                                )
-                            }
-                        }
-                    }
-
-
-                    Spacer(Modifier.padding(12.dp))
-
-                    val maxUsersString = remember {
-                        mutableStateOf("0")
-                    }
-                    CustomOutlinedTextInput(
-                        currentFocusRequester = maxUsers,
-                        onNext = { nameOfTraining.freeFocus() },
-                        keyBoardType = KeyboardType.Number,
-                        label = "Pocet ucastnikov",
-                        value = maxUsersString.value,
-                        onTextChanged = {
-                            maxUsersString.value = it
-                            maxUsersValue.intValue = it.toIntOrNull() ?: 0
-                        },
-                        enterButtonAction = ImeAction.Default,
-                        trailingIcon = {
-                            Icon(
-                                imageVector = Icons.Filled.People,
-                                contentDescription = "People count icon"
+                        trainings.forEach { training ->
+                            DropdownMenuItem(
+                                text = {
+                                    Text(
+                                        training.name,
+                                        style = MaterialTheme.typography.bodyLarge
+                                    )
+                                },
+                                onClick = {
+                                    textFieldState.value = training.name
+                                    chosenTrainingIndex = trainings.indexOf(training)
+                                    expanded = false
+                                },
+                                contentPadding = ExposedDropdownMenuDefaults.ItemContentPadding,
                             )
                         }
-                    )
-
-                    Spacer(Modifier.padding(12.dp))
-
-                    CustomOutlinedTextInput(
-                        currentFocusRequester = detailsOfTraining,
-                        onNext = { detailsOfTraining.freeFocus() },
-                        keyBoardType = KeyboardType.Text,
-                        label = "Detaily treningu",
-                        value = detailsValue.value,
-                        enterButtonAction = ImeAction.Default,
-                        onTextChanged = { detailsValue.value = it },
-                        supportingText = {
-                            NormalText("Nepovinne")
-                        },
-                        trailingIcon = {
-                            Icon(
-                                imageVector = Icons.Filled.EditNote,
-                                contentDescription = "Training details icon"
-                            )
-                        },
-                        singleLine = false,
-                        maxLines = 6,
-                        minLines = 6
-                    )
-
-                    Spacer(Modifier.padding(paddingValues))
+                    }
                 }
+
+
+                Spacer(Modifier.padding(12.dp))
+
+
+                CustomOutlinedTextInput(
+                    currentFocusRequester = maxUsers,
+                    onNext = { nameOfTraining.freeFocus() },
+                    keyBoardType = KeyboardType.Number,
+                    label = "Pocet ucastnikov",
+                    value = maxUsersString.value,
+                    onTextChanged = {
+                        maxUsersString.value = it
+                        maxUsersValue.intValue = it.toIntOrNull() ?: 0
+                    },
+                    enterButtonAction = ImeAction.Default,
+                    trailingIcon = {
+                        Icon(
+                            imageVector = Icons.Filled.People,
+                            contentDescription = "People count icon"
+                        )
+                    }
+                )
+
+                Spacer(Modifier.padding(12.dp))
+
+                CustomOutlinedTextInput(
+                    currentFocusRequester = detailsOfTraining,
+                    onNext = { detailsOfTraining.freeFocus() },
+                    keyBoardType = KeyboardType.Text,
+                    label = "Detaily treningu",
+                    value = detailsValue.value,
+                    enterButtonAction = ImeAction.Default,
+                    onTextChanged = { detailsValue.value = it },
+                    supportingText = {
+                        NormalText("Nepovinne")
+                    },
+                    trailingIcon = {
+                        Icon(
+                            imageVector = Icons.Filled.EditNote,
+                            contentDescription = "Training details icon"
+                        )
+                    },
+                    singleLine = false,
+                    maxLines = 6,
+                    minLines = 6
+                )
+                Spacer(Modifier.padding(paddingValues))
             }
         }
     }
 
+    if (showAlertDialog.value) {
+        CustomAlertDialog(
+            onDismissRequest = { showAlertDialog.value = false },
+            onConfirmation = {
+                showAlertDialog.value = false
+                onBackClick()
+            },
+            dialogTitle = "Upozornenie",
+            dialogContent = {
+                Row(Modifier.fillMaxWidth(), horizontalArrangement = Arrangement.Center) {
+                    NormalText("Treningove udaje budu stratene")
+                }
+            },
+            icon = {
+                Icon(
+                    imageVector = Icons.Filled.Warning,
+                    contentDescription = "Warning icon"
+                )
+            }
+        )
+    }
 }
 
 @RequiresApi(Build.VERSION_CODES.O)
